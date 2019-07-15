58f605814363e2c005ac4e32ce084d6d18488e47
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-15 19:59:56 +0800

    complete test3

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -48,8 +48,19 @@ public class CashRegisterTest {
     @Test
     public void should_verify_with_process_call_with_mockito() {
         //given
+        Printer printer = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(printer);
+        Item itemList[]=new Item[1];
+        for (int i = 0; i <1 ; i++) {
+            itemList[i]=new  Item("apple", 15.5);
+        }
+        Purchase purchase = new Purchase(itemList);
+
         //when
+        cashRegister.process(purchase);
+
         //then
+        verify(printer).print("apple"+"\t"+15.5+"\n");
     }
 
 }
